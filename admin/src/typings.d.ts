3df80935a5a2931052d33f69989d70cbb0d1cecc
wuchuheng <wuchuheng@163.com>
2020-09-07 18:33:16 +0800

    【admin】remove all

--- admin/src/typings.d.ts
+++
@@ -1,9 +0,0 @@
-/* SystemJS module definition */
-declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}
-declare module 'quill';
-declare module 'leaflet';
-declare module 'screenfull';
-declare module 'd3-shape';
